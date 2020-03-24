4fb8935e2bf91f4d626025678df49b93d207c4b0
parse-hardik <f20180062@hyderabad.bits-pilani.ac.in>
2020-03-24 19:30:48 +0530

    Notifs

--- src/Components/Home/NotifCard.js
+++ src/Components/Home/NotifCard.js
@@ -4,7 +4,7 @@ import './notifcard.css';
 const NotifCard =({ fromgname,tousername }) => {
 	return(
 		<div className="cardbg tc dib br3 pa3 ma2 grow bw2 shadow-5">
-			<img alt = "This a photu!" src = "https://robohash.org/${fromgname}?200x200" />
+			{/*<img alt = "This a photu!" src = "https://robohash.org/${fromgname}?200x200" />*/}
 			<div>
 				<h2>fromgname:{fromgname}</h2>
 				<h2>tousername:{tousername}</h2>
