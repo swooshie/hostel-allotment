661d2c200322cdf4526b564f2d8bc0dae9c743ba
Kriti <kriti.jethlia@yahoo.com>
2020-03-24 19:07:18 +0530

    groups card  showing group members

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import './card.css';
-
-const Card =({ name,key }) => {
+import { Button } from 'react-bootstrap';
+const Card =(props) => {
+	
 	return(
-		<div className="cardbg tc dib br3 pa3 ma2 grow bw2 shadow-5">
-			<img alt = "This a photu!" src = "https://robohash.org/${name}?200x200" />
-			<div>
-				<h2>{name}</h2>
-				<h3>{key}</h3>
-			</div>
+		<div className="tc dib br3 pa3 ma2  bw2  flip-card cardbg">
+		   <div className="flip-card-inner">
+				<div className="flip-card-front ">
+				<div>
+				 <img alt = "This a photu!" src = "https://robohash.org/${props.name}?200x200" />
+				</div>
+				<div>
+					<h2>{props.name}</h2>
+				</div>
+				</div>
+
+		        <div className="flip-card-back">
+		        <div>
+		         <h1>Members</h1>
+		        {
+		        	props.member.map((user,i)=>(
+		        		<h3> {user.name} </h3>
+		        	))
+		        }
+		        </div>
+		        <div className='btn'>
+		        <Button variant="dark">Join Group</Button>
+		        </div>
+		        </div>
+			</div>	
 		</div>
 	);
 }
